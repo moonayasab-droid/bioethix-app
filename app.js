99f8426d82aa3cd1897d1708875dc0bb63501afa
moonayasab-droid <moonayasab@gmail.com>
2026-08-17 11:37:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -556,7 +556,7 @@ function submitFeedback(event) {
   document.getElementById("feedback-form").reset();
 }
 
-caseStudies.push( 
+const additionalCaseStudies = [
   {
     id: "surgical-robotics",
     title: "Autonomous Surgical Robotics in Dental Practice",
